9bb5887ce20080f3242cdee77ec427beb2ef2d8a
bertuja <40971787+bertuja@users.noreply.github.com>
2022-05-08 20:42:36 -0300

    soy el capo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola dbyte!'));
+app.get('/', (req, res) => res.send('Hola dbyte!esperemos por fin aprendas algo'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
